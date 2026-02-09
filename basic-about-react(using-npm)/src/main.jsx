0e6c84bbea430a442573cc73befb13a758670f76
Aditya <adityamaddheshiya669@gmail.com>
2026-02-10 00:01:00 +0530

    added

--- basic-about-react(using-npm)/src/main.jsx
+++ basic-about-react(using-npm)/src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+
+function myApp(){
+  return(
+    <>
+    <h1>Customm APP !</h1>
+    </>
+  )
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
