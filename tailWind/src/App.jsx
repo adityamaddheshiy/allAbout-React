3d4213762b35bc5f2ccb5635d66e38dd14aed6d5
Aditya <adityamaddheshiya669@gmail.com>
2026-02-22 09:20:53 +0530

    tailwind installation

---
+++ tailWind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className=' h-screen w-full bg-gray-700'>
+      <>
+      {/* <H1>APP</H1> */}
+      </>
+    </div>
+  )
+}
