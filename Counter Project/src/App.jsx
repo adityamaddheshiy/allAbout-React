22aae17f0ae88aab9ed40851f5ab4d8e836f706e
Aditya <adityamaddheshiya669@gmail.com>
2026-04-05 18:19:53 +0530

    update

--- Counter Project/src/App.jsx
+++ Counter Project/src/App.jsx
@@ -2,25 +2,33 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [Num,setNum]=useState(0);
-  function increase(){
-    setNum(Num+1);
+  const [Num, setNum] = useState(0);
+  function increase() {
+    setNum(Num + 1);
   }
 
-  function decrease(){
-    setNum(Num-1)
+  function decrease() {
+    setNum(Num - 1)
   }
 
-  function jump5num(){
-    setNum(Num+5);
+  function jump5num() {
+    setNum(Num + 5);
+  }
+
+
+  function decreaseby5() {
+    setNum(Num - 5);
   }
   return (
 
     <div>
       <h1 > {Num} </h1>
-      <button onClick={increase}>increase</button>
-      <button onClick={decrease}>decrease</button>
-      <button onDoubleClick={jump5num}>increase BY 5</button>
+      <div className="btn">
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={jump5num}>Increase BY 5</button>
+        <button onClick={decreaseby5}>Decrease By 5</button>
+      </div>
     </div>
   )
 }
